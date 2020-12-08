c5aa6f53cf0d4b0ddf8460597d4d7ab1c2b8a139
Kirill Samokhvalov <ksamokhvalov@340basics.com>
2020-12-09 00:10:23 +0300

    Initial commit

---
+++ AzureServiceBusObserver/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AzureServiceBusObserver.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+        public string Yo => "Yo";
+        public string ConnectionString { get; set; }
+    }
+}
---
+++ AzureServiceBusObserver/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace AzureServiceBusObserver.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
